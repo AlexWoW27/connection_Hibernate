78254a6c597030999395b9f794dc27e2b748f3a5
Elena <sokolovaelenalove@gmail.com>
2023-05-24 20:14:32 +0300

    task add connection

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,33 +2,42 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
     public UserDaoJDBCImpl() {
     }
+
     @Override
     public void createUsersTable() {
-        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS Person(id int PRIMARY KEY AUTO_INCREMENT, name varchar(30), lastname varchar(30), age TINYINT)")) {
+        try(Connection connection = Util.getConnection();
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("CREATE TABLE IF NOT EXISTS Person(id int PRIMARY KEY AUTO_INCREMENT," +
+                            " name varchar(30), lastname varchar(30), age TINYINT)")) {
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
     @Override
     public void dropUsersTable() {
-        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement("DROP TABLE IF EXISTS Person")) {
+        try(Connection connection = Util.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("DROP TABLE IF EXISTS Person")) {
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement("INSERT INTO Person(name, lastName, age) VALUES (?,?,?)")) {
+        try(Connection connection = Util.getConnection();
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("INSERT INTO Person(name, lastName, age) VALUES (?,?,?)")) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
@@ -38,19 +47,23 @@ public class UserDaoJDBCImpl implements UserDao {
             e.printStackTrace();
         }
     }
+
     @Override
     public void removeUserById(long id) {
-        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement("DELETE FROM Person WHERE id = ?")) {
+        try(Connection connection = Util.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Person WHERE id = ?")) {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
     @Override
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
-        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement("SELECT * FROM Person")) {
+        try(Connection connection = Util.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Person")) {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 User user = new User();
@@ -63,12 +76,13 @@ public class UserDaoJDBCImpl implements UserDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        System.out.println(userList);
         return userList;
     }
+
     @Override
     public void cleanUsersTable() {
-        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement("TRUNCATE TABLE Person")) {
+        try(Connection connection = Util.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("TRUNCATE TABLE Person")) {
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,24 +8,14 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/kata_base";
     private static final String USERNAME = "kata";
     private static final String PASSWORD = "postgres";
-
     private static Connection connection;
 
     public static Connection getConnection() {
-        return connection;
-    }
-
-    static  {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
         try {
             connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return connection;
     }
 }
